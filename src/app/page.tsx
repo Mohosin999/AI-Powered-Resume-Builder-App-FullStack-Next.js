e437c3eb6dfb5989022af12673ea724ee7436d81
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-07-13 19:23:39 +0600

    authentication setup completed
    For authentication, I used clerk here.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import React from "react";
+import { currentUser } from "@clerk/nextjs/server";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = async () => {
+  const user = await currentUser();
+  console.log("----------------", user?.emailAddresses[0].emailAddress);
+
+  return (
+    <div>
+      <h1>This is home page</h1>
+      <br />
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton mode="modal">
+          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default Home;
