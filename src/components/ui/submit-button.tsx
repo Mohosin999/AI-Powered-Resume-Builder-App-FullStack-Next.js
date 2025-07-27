fcd374fefe867d4b2771fa369905efef22502e0b
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-07-27 07:21:07 +0600

    added success icon on submit button

--- src/components/ui/submit-button.tsx
+++ src/components/ui/submit-button.tsx
@@ -1,7 +1,47 @@
+// "use client";
+// import { Button } from "@/components/ui/button";
+// import { useFormStatus } from "react-dom";
+// import { Loader2 } from "lucide-react";
+
+// interface SubmitButtonProps {
+//   pendingText?: string;
+//   defaultText?: string;
+//   className?: string;
+// }
+
+// export function SubmitButton({
+//   pendingText = "Submitting...",
+//   defaultText = "Submit",
+//   className = " ",
+// }: SubmitButtonProps) {
+//   const { pending } = useFormStatus();
+
+//   return (
+//     <Button
+//       disabled={pending}
+//       type="submit"
+//       variant="outline"
+//       className={`text-gray-900 hover:bg-emerald-400 hover:border-emerald-400 cursor-pointer ${className}`}
+//     >
+//       {pending ? (
+//         <>
+//           <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+//           {pendingText}
+//         </>
+//       ) : (
+//         <>{defaultText}</>
+//       )}
+//     </Button>
+//   );
+// }
+
 "use client";
 import { Button } from "@/components/ui/button";
 import { useFormStatus } from "react-dom";
 import { Loader2 } from "lucide-react";
+import { FaCircleCheck } from "react-icons/fa6";
+
+import { useEffect, useState } from "react";
 
 interface SubmitButtonProps {
   pendingText?: string;
@@ -12,9 +52,19 @@ interface SubmitButtonProps {
 export function SubmitButton({
   pendingText = "Submitting...",
   defaultText = "Submit",
-  className = " ",
+  className = "",
 }: SubmitButtonProps) {
   const { pending } = useFormStatus();
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  useEffect(() => {
+    // when pending becomes false (submission done), show ✅
+    if (!pending) {
+      setShowSuccess(true);
+      const timer = setTimeout(() => setShowSuccess(false), 5000);
+      return () => clearTimeout(timer); // cleanup
+    }
+  }, [pending]);
 
   return (
     <Button
@@ -25,8 +75,13 @@ export function SubmitButton({
     >
       {pending ? (
         <>
-          <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+          <Loader2 className="w-4 h-4 mr-1 animate-spin" />
           {pendingText}
+        </>
+      ) : showSuccess ? (
+        <>
+          <FaCircleCheck className="text-green-600 mr-1 " />
+          Success
         </>
       ) : (
         <>{defaultText}</>
