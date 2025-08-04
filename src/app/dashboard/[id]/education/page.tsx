a320b07e0c145b6d5d840d069510fc2557ee1c2d
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-08-04 08:15:16 +0600

    trying to debug

--- src/app/dashboard/[id]/education/page.tsx
+++ src/app/dashboard/[id]/education/page.tsx
@@ -1,26 +1,13 @@
-// import { getEducations } from "@/actions/resume-actions";
-// import EducationPageClient from "./education-page-client";
-
-// interface EducationPageProps {
-//   params: { id: string };
-// }
-
-// export default async function EducationPage({ params }: EducationPageProps) {
-//   console.log("params", typeof params.id);
-//   const educations = await getEducations(params.id);
-
-//   return <EducationPageClient educations={educations} resumeId={params.id} />;
-// }
-
 import { getEducations } from "@/actions/resume-actions";
 import EducationPageClient from "./education-page-client";
 
-interface EducationPageProps {
-  params: Promise<{ id: string }>;
-}
-
-export default async function EducationPage({ params }: EducationPageProps) {
+export default async function EducationPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const { id } = await params;
+
   const educations = await getEducations(id);
   return <EducationPageClient educations={educations} resumeId={id} />;
 }
