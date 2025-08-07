0640dd81f8ce2e1f43c7e2a9972b189b1df3e0e6
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-08-07 09:07:24 +0600

    tying to add animation

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -63,3 +63,28 @@ export const features = [
       "Download your resume in a high-quality PDF file instantly with a single click, ready for job applications.",
   },
 ];
+
+/**
+ * ========================= Framer Motion Variants =========================
+ * Animation Variants
+ */
+export const fadeInUp = {
+  initial: { opacity: 0, y: 100 },
+  whileInView: { opacity: 1, y: 0 },
+  transition: { duration: 0.6 },
+  viewport: { once: true, amount: 0 },
+};
+
+export const fadeInLeft = {
+  initial: { opacity: 0, x: -100 },
+  whileInView: { opacity: 1, x: 0 },
+  transition: { duration: 0.6 },
+  viewport: { once: true, amount: 0 },
+};
+
+export const fadeInRight = {
+  initial: { opacity: 0, x: -100 },
+  whileInView: { opacity: 1, x: 0 },
+  transition: { duration: 0.6 },
+  viewport: { once: true, amount: 0 },
+};
