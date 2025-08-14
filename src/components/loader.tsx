e8d80c8e20094abca5f90a9ac4101b06d7d2a5f8
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-08-14 15:37:37 +0600

    refactor: loading icon fit on the center

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Loader() {
   return (
-    <div className="flex justify-center items-center gap-2 h-[100vh] ">
+    <div className="fixed inset-0 flex justify-center items-center bg-white dark:bg-[#131A25] z-50">
       <div className="flex justify-center items-center gap-2 animate-pulse">
         <Image src="/logo.png" alt="AI Resume Logo" width={40} height={40} />
         <span className="text-emerald-500 text-xl font-semibold tracking-wide">
