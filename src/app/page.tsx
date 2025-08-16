b9c0d30c39633dfef00279c508cdc0e1d3ad8279
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-08-16 09:39:08 +0600

    test: setup completed for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
-import React from "react";
-import HomePage from "@/components/home-page";
+// import React from "react";
+// import HomePage from "@/components/home-page";
 
-const Home = async () => {
+// const Home = async () => {
+//   return (
+//     <div>
+//       <HomePage />
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+import Link from "next/link";
+
+export default function Home() {
   return (
     <div>
-      <HomePage />
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
     </div>
   );
-};
-
-export default Home;
+}
